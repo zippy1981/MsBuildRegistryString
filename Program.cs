dd836cffc2fb765d9ed7518fa50826651a2e250b
Justin Dearing <zippy1981@gmail.com>
2012-12-19 08:18:47 -0500

    Just a reminder

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace MsBuildRegistryString
     {
         static void Main()
         {
+            Console.WriteLine("Its not about the code, its about the MSBuild file.");
+            Console.WriteLine("");
             Console.Write("Press any key to continue . . .");
             Console.ReadKey(false);
             Console.WriteLine();
